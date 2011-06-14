cdaa117c9a66898f912bc7c0e08c5d42b210909c
Aaron Hicks <hicksa@landcareresearch.co.nz>
2011-06-14 16:51:17 +1200

    Added grisu-jython.jar and importing modules into submitworld.py

--- HelloWorld/submitworld.py
+++ HelloWorld/submitworld.py
@@ -4,5 +4,15 @@ Created on 14/06/2011
 @author: hicksa
 '''
 
-if __name__ == '__main__':
-    pass
+import sys
+
+# Launch this script with: java -jar ../lib/grisu-jython.jar submitworld.py
+# otherwise, you will need to append the grisu-jython.jar to the path
+# sys.path.append('../lib/grisu-jython.jar')
+from grisu.jcommons.constants import Constants
+from grisu.frontend.control.login import LoginManager
+from grisu.frontend.model.job import JobObject, BatchJobObject, JobsException, BackendException, FileUploadException
+from grisu.model import FileManager
+from grisu.settings import ClientPropertiesManager
+from grisu.control import ResubmitPolicy, ResubmitPolicy, DefaultResubmitPolicy
+
