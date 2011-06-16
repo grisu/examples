f62ba37d4116b2b170690f0690c2f71f6e5a33b9
Aaron Hicks <hicksa@landcareresearch.co.nz>
2011-06-16 15:15:31 +1200

    Jobs are now defined, created and submitted.

--- HelloWorld/submitworld.py
+++ HelloWorld/submitworld.py
@@ -7,10 +7,13 @@ Created on 14/06/2011
 import sys
 import random
 import os
+import time
 
 # Launch this script with: java -jar ../lib/grisu-jython.jar submitworld.py
 # otherwise, you will need to append the grisu-jython.jar to the path
 # sys.path.append('../lib/grisu-jython.jar')
+
+# Documentation on these objects can be found here: http://grisu.github.com/grisu/javadoc/
 from grisu.jcommons.constants import Constants
 from grisu.frontend.control.login import LoginManager
 from grisu.frontend.model.job import JobObject, JobsException, BackendException, FileUploadException
@@ -19,14 +22,23 @@ from grisu.settings import ClientPropertiesManager
 from grisu.control import ResubmitPolicy, ResubmitPolicy, DefaultResubmitPolicy
 
 # If your netwok has a HTTP proxy you can configure Grisu-Jython to use it here
-# from grisu.jcommons.utils import HttpProxyManager
+from grisu.jcommons.utils import HttpProxyManager
 # The arguments are: address, port, username and password.
-# HttpProxyManager.setHttpProxy("proxy.example.com", 8080, "", "")
+#HttpProxyManager.setHttpProxy("proxy.example.com", 8080, "", "")
+HttpProxyManager.setHttpProxy("202.27.240.31", 8080, "", "")
 
 # Set the backend to which jobs will be submitted
 backend = "BeSTGRID"
 
-# NOTE: from this point on, some comments will
+# Set the group under which this job is submitted
+# these are also called "Virtual Organisation" or a VO
+group= "/ARCS/BeSTGRID"
+
+# We need an absolute path to the local directory
+current_dir = os.path.abspath(os.path.curdir)
+
+# NOTE: from this point on, some comments will be in the form of notification output
+# as this is an example script, it's pretty verbose
 
 print "INFO: Creating service inteface to " + backend
 service_interface = LoginManager.loginCommandline(backend)
@@ -39,6 +51,58 @@ job_count=5
 base_job_name = str(random.randint(10000,99999))+'-hello-'
 print "INFO: Base job name is "+base_job_name
 
+# There are three stages, creating the jobs, submitting the jobs, then after they have finished, retrieving the job outputs
+
+# Createing a list of jobs
+jobs=[]
+print "INFO: Defining "+str(job_count)+" helloworld jobs"
+for i in range(1,job_count+1):
+    print "INFO: Defining job "+str(i)+" of "+str(job_count)
+    #The next lines create the actual jobs
+    job=JobObject(service_interface)                # Create a job
+    job.setJobname(base_job_name+str(i))            # Give it a (hopefully) unique name
+    job.addInputFileUrl(os.path.join(current_dir,"helloworld.py"))  # Add a file
+    job.setCommandline("python helloworld.py")      # Set the command to be run
+    print"INFO: job "+job.getJobname()+" defined"
+    jobs.append(job)
+
+# Submit the jobs to be run
+# Note the exception catching to give more information about a job failing
+for job in jobs:
+    time_start = time.time()
+    try:
+        print "INFO: Creating job "+job.getJobname()+" on "+backend+" backend, with "+group+" group"
+        job.createJob(group)
+    except (JobsException), error:
+        print "HALT: Exception submitting job!"
+        print "Job: "+job.getJobname()+", Error: "+error.getFailures().get(job).getLocalizedMessage()
+        sys.exit(1)
+    except (BackendException), error:
+        print "HALT: Exception from grisu backend!"
+        print error.getLocalizedMessage()
+        print"========================"
+        time.sleep(3)
+        error.printStackTrace()
+        sys.exit(1)
+        
+    try:
+        print "INFO: Submitting job "+job.getJobname()
+        job.submitJob()
+    except (JobsException), error:
+        print "HALT: Exception submitting job!"
+        print "Job: "+job.getJobname()+", Error: "+error.getFailures().get(job).getLocalizedMessage()
+        sys.exit(1)
+    except (BackendException), error:
+        print "HALT: Exception from grisu backend!"
+        print error.getLocalizedMessage()
+        print"========================"
+        time.sleep(3)
+        error.printStackTrace()
+        sys.exit(1)
+
+    time_elapsed = time.time() - time.start
+    print "INFO: Job submission for "+job.getJobname()+" took "+str(time_elapsed)+" seconds"
+
 # Create an output directory
 output_dir=base_job_name+'output'
 try:
@@ -47,18 +111,6 @@ try:
 except:
     print "HALT: Could not create output directory "+output_dir
     sys.exit(1)
-
-# Createing a list of jobs
-jobs=[]
-print "INFO: Creating "+str(job_count)+" helloworld jobs"
-for i in range(1,job_count+1):
-    print "INFO: Creating job "+str(i)+" of "+str(job_count)
-    job=JobObject(service_interface)
-    job.setJobname(base_job_name+str(i))
-    job.addInputFileUrl("helloworld.py")
-    job.setCommandline("python helloworld.py")
-    print"INFO: job "+job.getJobname()+" created"
-    jobs.append(job)
-    
+        
 print "EXIT: submitworld.py complete."
 
