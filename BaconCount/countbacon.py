5270b6c621cd69c5775d71ff57217634f6adf005
Aaron Hicks <hicksa@landcareresearch.co.nz>
2011-06-17 16:50:50 +1200

    Beginning to count bacon

--- BaconCount/countbacon.py
+++ BaconCount/countbacon.py
@@ -4,5 +4,30 @@ Created on 14/06/2011
 @author: hicksa
 '''
 
-if __name__ == '__main__':
-    pass
+import sys
+import os
+
+current_dir = os.path.abspath(os.path.curdir)
+
+
+# First check that the script is called correctly
+if (len(sys.argv[1:]) != 2):
+    print "HALT: Incorrect usage!"
+    print "Requires arguments for dictionary and input files"
+    print "e.g. java -jar ../lib/grisu-jython.jar countbacon.py dictionary.txt input/file.txt"
+    sys.exit(1)
+
+# Assign arguments and test they're valid
+dictionary_path, input_path = sys.argv[1:]
+print "Dictionary: "+dictionary_path
+print "Input:      "+input_path
+
+if not os.path.isfile(dictionary_path):
+    print "HALT: Dictionary file "+dictionary_path+" is not a file"
+    sys.exit(1)
+
+if not os.path.isfile(input_path):
+    print "HALT: Input file "+input_path+" is not a file"
+    sys.exit(1)
+
+
